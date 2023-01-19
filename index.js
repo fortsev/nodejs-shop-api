a294440978ff4aa71781c6832f0821a7ee015df9
Nikita Semin <30572809+fortsev@users.noreply.github.com>
2023-01-19 14:56:07 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const express = require('express');
+const sequelize = require('./dbcon');
+const models = require('./models/models');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({
+
+}));
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Ok'})
+})
+
+//Обработка ошибок
+app.use(errorHandler);
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`server start ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
